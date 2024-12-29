826a884b3e9de5d27e867c3d21e9e5e584323df5
Harvan Nurluthfi <harvan.nurluthfi@gmail.com>
2024-12-30 02:43:18 +0700

    chore: update readme

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -13,7 +13,7 @@
 <body class="bg-dark">
     <section class="bg-light ">
         <div class="container text-center ">
-            <h1 class="p-4"><strong>Giant Book Supplier</strong></h1>
+            <h1 class="p-4"><strong>Manga Library</strong></h1>
         </div>
     </section>
     <div class="container mt-4">
@@ -21,8 +21,8 @@
         @yield('content')
     </div>
     <div class="text-center bg-light py-4 mt-5">
-        © 2022 Copyright
-        <span class="text-reset fw-bold">UTS WebProg 2022</span>
+        © 2022 Made with ❤️ by
+        <span class="text-reset fw-bold">Harvan Nurluthfi</span>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
